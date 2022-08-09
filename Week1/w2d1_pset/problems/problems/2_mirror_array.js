f873214f02706e18086fc4bfa225822b29ecf96a
“Gihae <designbygihae@gmail.com>
2022-08-09 01:57:56 -0400

    update

---
+++ Week1/w2d1_pset/problems/problems/2_mirror_array.js
@@ -0,0 +1,27 @@
+/***********************************************************************
+Write a function `mirrorArray(array)` that takes in an array as an
+argument and returns a new array "mirrored" as shown in the examples:
+
+Examples:
+
+mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+***********************************************************************/
+
+function mirrorArray(array) {
+    let arr = [];
+
+    for (let i = 0; i < array.length; i += 1) {
+        arr.push(array[i]);
+    }
+    for (let i = array.length - 1; i >= 0; i -= 1) {
+        arr.push(array[i]);
+    }
+    return arr;
+}
+
+console.log(mirrorArray([1, 2, 3]));
+console.log(mirrorArray(['a', 'b', 'c', 'd']));
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = mirrorArray;
