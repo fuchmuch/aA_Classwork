4bdacb96554ab6490bce5883cd24258ee3a83c6a
“Gihae <designbygihae@gmail.com>
2022-08-10 12:02:17 -0400

    update

--- Week2/w2d2_pset/problem_set/problems/6_adults.js
+++ Week2/w2d2_pset/problem_set/problems/6_adults.js
@@ -16,8 +16,25 @@ adults(ppl); // => [ 'John', 'Jane' ]
 ***********************************************************************/
 
 function adults(people) {
+  let names = [];
+  for (let i = 0; i < people.length; i += 1) {
+    let person = people[i];
+    console.log(person);
 
+    if (person.age >= 18) {
+      names.push(person.name) //using dot notation and person.name is string
+    }
+  };
+  return names
 }
 
+let ppl = [
+  { name: 'John', age: 20 },
+  { name: 'Jim', age: 13 },
+  { name: 'Jane', age: 18 },
+  { name: 'Bob', age: 7 }
+];
+
+console.log(adults(ppl));
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = adults;
